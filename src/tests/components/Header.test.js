cd349ac7997c297ca05f4e96b1c2abfc30a6481c
salaz310 <lrso.10@hotmail.com>
2021-01-10 22:41:40 -0600

    Implementing components testing

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,9 @@
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import React from 'react';
+import Header from '../../components/Header';
+
+test('Should render Header correctly',()=>{
+    const renderer= new ReactShallowRenderer();
+    renderer.render(<Header />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
