d09495e198cc4077a538244ec4b835106e7f6f68
salaz310 <lrso.10@hotmail.com>
2021-01-14 15:58:32 -0600

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const path= require('path');
 
 const publicPath= path.join(__dirname,'..','public');
 
+const port= process.env.PORT || 3000;
+
 
 app.use(express.static(publicPath));
 
@@ -11,6 +13,6 @@ app.get('*',(req,res)=>{
     res.sendFile(path.join(publicPath,'index.html'));
 });
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server is up on port 3000');
 });
