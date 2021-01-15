b12682ef5dd5ebe0bba18e269f7974b0bb5bbb20
salaz310 <lrso.10@hotmail.com>
2021-01-14 22:05:34 -0600

    Setup formatting for amount and CreatedAt

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
             //AQUI SE ESTA HACIENDO UN SPREAD (...) DEL OBJETO, PERO TAMBIEN SE PUEDE LLAMAR A "DISPATCH"
 const ExpenseListItem= ({description,amount,createdAt,id})=>(
     <div>
     <h3>{description}</h3>
-        <p>Amount: {amount}</p>
-        <p>Created At: {createdAt}</p>
+        <p>Amount: {numeral(amount/100).format('$0,0.00')}</p>
+        <p>Created At: {moment(createdAt).format('MMMM Do, YYYY')}</p>
         <Link to={`/edit/${id}`} >Edit</Link>
     </div>
 );
